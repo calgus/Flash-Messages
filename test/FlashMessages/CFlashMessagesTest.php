6e748ba22b3f2cbd3fd3487f691c7fb994f612a0
Non Nill <cagu15@student.bth.se>
2015-10-13 18:50:15 +0200

    Added getMessage and getSessionMessage to test

--- test/FlashMessages/CFlashMessagesTest.php
+++ test/FlashMessages/CFlashMessagesTest.php
@@ -15,7 +15,7 @@ class CFlashMessagesTest extends \PHPUnit_Framework_TestCase
      * @return void
      *
      */
-    public function testGetNameFromSession()
+    public function testGetSessionMessageClean()
     {
         $flash = new \Anax\FlashMessages\CFlashMessages();
         $di    = new \Anax\DI\CDIFactoryDefault();
@@ -37,7 +37,65 @@ class CFlashMessagesTest extends \PHPUnit_Framework_TestCase
         $messages = $flash->getSessionMessageClean();
         foreach ($messages as $index => $value) {
             $this->assertEquals($message, $value['message'], "Form element value missmatch, method.");
-        }
-        
+        }    
+    }
+    
+    /**
+     * Test
+     *
+     * @return void
+     *
+     */
+    public function testGetSessionMessage()
+    {
+        $flash = new \Anax\FlashMessages\CFlashMessages();
+        $di    = new \Anax\DI\CDIFactoryDefault();
+        $flash->setDI($di);
+
+        $di->setShared('session', function () {
+            $session = new \Anax\Session\CSession();
+            $session->configure(ANAX_APP_PATH . 'config/session.php');
+            $session->name();
+            //$session->start();
+            return $session;
+        });
+
+        $message = "testing message";
+        $flash->errorMessage($message);
+        $flash->warningMessage($message);
+        $flash->noticeMessage($message);
+        $flash->successMessage($message);
+        $messages = $flash->getSessionMessageClean();
+        $output = "<div class='flashmessages'>";
+        foreach ($messages as $index => $value) {
+                $type = $value['type'];
+                $message = $value['message'];
+                $output .= "<p class='{$type} messages'>";
+                $output .= $type;
+                $output .= ": ";
+                $output .= $message;
+                $output .= "</p>";
+        }    
+        $output .= "</div>";
+        $name2 = $flash->getSessionMessage();
+        $this->assertEquals($output, $name2, "Form element value missmatch, method.");
+    }
+    
+    /**
+     * Test
+     *
+     * @return void
+     *
+     */
+    public function testGetMessage()
+    {
+        $flash = new \Anax\FlashMessages\CFlashMessages();
+        $di    = new \Anax\DI\CDIFactoryDefault();
+        $flash->setDI($di);
+
+        $message = "testing message";
+        $flash->setMessage($message);
+        $messages = $flash->getMessage();
+        $this->assertEquals($message, $messages, "Form element value missmatch, method.");   
     }
 }
